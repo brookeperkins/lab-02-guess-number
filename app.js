609fc19e41ede5e66ef34489809cfe8a3bdc0a1b
brookeperkins <brookemmperkins@gmail.com>
2020-07-07 21:56:22 -0700

    added JS

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 // import functions and grab DOM elements
-import { compareNumbers } from './compare.js';
+import compareNumbers from './compare.js';
 
-const myButton = document.querySelector('#button');
 const myGuess = document.querySelector('#guess');
-const results = document.querySelector('#results');
-const triesLeft = document.querySelector('#')
+const myButton = document.querySelector('#button');
+const guessWas = document.querySelector('#guess-was');
+const winLose = document.querySelector('#win-lose');
+const triesRemaining = document.querySelector('#tries-remaining');
+const reset = document.querySelector('#reset');
+
+console.log(myGuess, myButton, guessWas, winLose, triesRemaining, reset);
+
 // initialize state
+let triesLeft = 4;
 
 // set event listeners to update state and DOM
